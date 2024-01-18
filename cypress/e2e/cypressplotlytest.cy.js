97f00a559abbb09e656be1f903bebf8cb3fd080c
Jashan Singh <sekhonjashan@gmail.com>
2024-01-18 15:34:23 +0530

    Test case 2 created and verified

--- cypress/e2e/cypressplotlytest.cy.js
+++ cypress/e2e/cypressplotlytest.cy.js
@@ -1,4 +1,8 @@
+
+
 describe('Plotly Assignment Test Cypress.io', () => {
+  
+  
   it('Visit the website and scoll to find number of downloads', () => {
     cy.visit('https://www.cypress.io/')
 
@@ -7,5 +11,22 @@ describe('Plotly Assignment Test Cypress.io', () => {
     // finding the element <div class="font-primary text-gray-700 text-[18px] leading-[28px] font-normal">Weekly downloads</div>
 
     cy.get('.grow').contains('5M+').should('have.class','font-bold text-gray-1000 text-[28px] leading-[40px] font-primary')
+  })
+
+
+  //Test Case - 2 
+ //dropdownCompany
+  it('About Page from dropdown',()=>{
+    cy.visit('https://www.cypress.io/')
+
+    cy.get('#dropdownCompany').trigger('mouseover');
+
+    cy.contains('About Cypress').should('be.visible');
+    
+    cy.contains('About Cypress').click();
+
+    cy.url().should('include','/about-us');
+
+    cy.get('#story > span > a').should('exist');
   })
 })
