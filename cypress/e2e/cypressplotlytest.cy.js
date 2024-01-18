828fba533a2e3703b3cf6753ed8fe02eebff5f9d
Jashan Singh <sekhonjashan@gmail.com>
2024-01-19 01:44:17 +0530

    Code correction and refactoring

--- cypress/e2e/cypressplotlytest.cy.js
+++ cypress/e2e/cypressplotlytest.cy.js
@@ -1,6 +1,6 @@
 /**
  * @author Jashan Singh
- * @version 1.9.1
+ * @version 1.9.2
  * 
  * This is the main file that includes all the test cases.
  * 
@@ -10,10 +10,10 @@
 const aboutCypressSelector = 'About Cypress';
 const dropdownCompany = '#dropdownCompany';
 
-// Test Case 1: Verify Website Visit and Scroll Functionality
 describe('Cypress Test', () => {
   beforeEach(() => {
     cy.visit('/');
+   // cy.contains('main section:nth-child(3) img:nth-child(9)').should('be.visible');
   });
 
   afterEach(() => {
@@ -21,42 +21,43 @@ describe('Cypress Test', () => {
     cy.clearLocalStorage();
   });  
 
+  // Test Case 1: Verify Website Visit and Scroll Functionality
   it('Visit the website and scroll to find number of downloads', () => {
     cy.contains('Loved by OSS, trusted by Enterprise').scrollIntoView();
-    cy.get('.grow').contains('5M+');
     cy.contains("Weekly downloads").siblings("div").first().should('have.text', '5M+');
   });
 
+// Test Case 2: Navigation to 'About Cypress' Page
   it('About Page from dropdown', () => {
     cy.get(dropdownCompany).trigger('mouseover').should('be.visible',{timeout:1000});
-    cy.contains(aboutCypressSelector).should('be.visible', { timeout: 15000 });
-    cy.contains(aboutCypressSelector).click();
+    cy.contains(aboutCypressSelector).should('be.visible',{timeout:15000});
+    cy.contains(aboutCypressSelector).click({ force: true });
     cy.url().should('include', '/about-us');
   });
 
+// Test Case 3: Validate 'npm install cypress' Command
   it('Users can click on "npm install cypress" and verify the copied text', () => {
-    cy.contains('button', 'Install').click();
+    cy.contains('button', 'Install').click({ force: true });
     cy.get('nav dialog .overflow-y-auto').should('be.visible');
-    cy.contains('button', 'npm install cypress').click();
-    cy.wait(1000);
-    cy.contains('button', 'npm install cypress').should('be.visible');
-    cy.wait(200);
+    cy.contains('button', 'npm install cypress').click({force:true});
     cy.readClipboard().then((text) => {
         expect(text).to.eq('npm install cypress --save-dev');
         cy.log('Copied text:', text);
     });
   });
 
+//Test Case 4: Accessing 'Visual Review' Under 'Product'
   it('User click on Product and then Visual reviews', () => {
     cy.hoverOnProduct();
-    cy.contains('Visual Reviews').should('be.visible', { timeout: 15000 }).click();
+    cy.contains('Visual Reviews').should('be.visible', { timeout: 15000 }).click({force:true});
     cy.contains('Review and debug failures visually').should('be.visible');
     cy.get('video source[src*="PullRequestReview"]').parent('video').should('exist');
   });
 
+//Bonus Test Case 5: Smart Orchestration to Test Analytics
   it('Product to Smart Orchestration to Test Analytics', () => {
     cy.hoverOnProduct();
-    cy.contains('Smart Orchestration').should('be.visible', { timeout: 15000 }).click();
+    cy.contains('Smart Orchestration').should('be.visible', { timeout: 15000 }).click({force:true});
     cy.get('#test_analytics').scrollIntoView();
     cy.get('a[href="#test_analytics"].border-jade-200').should('be.visible');
   });
